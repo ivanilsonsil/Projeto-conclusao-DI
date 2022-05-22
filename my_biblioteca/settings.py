da9b491a5aa6c1100ea326565ea0333a9234b15e
projetosete <etearianovilarsuassuna1@gmail.com>
2022-05-22 15:03:43 -0300

    settings

--- my_biblioteca/settings.py
+++ my_biblioteca/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = [True]#config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []#['fxbiblioteca300.herokuapp.com','localhost']
 
 
 # Application definition
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'America/Pernambuco'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
